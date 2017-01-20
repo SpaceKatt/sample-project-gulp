be0750a62027ebb16b068719b3c5e637c0a33d64
Thomas Kercheval <spacekattpoispin@gmail.com>
2017-01-20 13:30:45 -0800

    Add default build task & standard lint

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,29 +1,44 @@
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
-var less = require('gulp-less');
-var minifyCSS = require('gulp-cssnano');
-var prefix = require('gulp-autoprefixer');
+var gulp = require('gulp')
+var standard = require('gulp-standard')
+var uglify = require('gulp-uglify')
+var concat = require('gulp-concat')
+var less = require('gulp-less')
+var minifyCSS = require('gulp-cssnano')
+var prefix = require('gulp-autoprefixer')
+var del = require('del')
 
-gulp.task('scripts', function() {
-  return gulp.src('app/scripts/**/*.js')
-    .pipe(concat('main.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/scripts'));
-});
+gulp.task('clean', function () {
+  return del(['dist'])
+})
 
-gulp.task('styles', function() {
+gulp.task('styles', function () {
   return gulp.src('app/styles/main.less')
     .pipe(less())
     .pipe(minifyCSS())
     .pipe(prefix())
-    .pipe(gulp.dest('dist/styles'));
-});
+    .pipe(gulp.dest('dist/styles'))
+})
 
 gulp.task('test', function () {
   return gulp.src(['app/scripts/**/*.js', '!app/scripts/vendor/**/*.js'])
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish', { verbose: true }))
-    .pipe(jshint.reporter('fail'));
-});
+    .pipe(standard())
+    .pipe(standard.reporter('default', {
+      breakOnError: true,
+      quiet: true
+    }))
+})
+
+gulp.task('scripts',
+  gulp.series('test', function scriptsInternal () {
+    return gulp.src('app/scripts/**/*.js')
+      .pipe(concat('main.min.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('dist/scripts'))
+  })
+)
+
+gulp.task('default',
+  gulp.series('clean',
+    gulp.parallel('styles', 'scripts')
+  )
+)
